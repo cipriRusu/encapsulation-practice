4e5a7620d7b7561edc3215997ab2ff2730a4aee8
cipri <cipri91rusu@gmail.com>
2024-04-25 23:21:25 +0200

    Initial commit bulk of files

---
+++ Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace EncapsulationProj
+{
+    internal class Student
+    {
+        private readonly string studentName;
+        private Subject[] studentSubjects = new Subject[0];
+
+        public Student(string inputStudentName)
+        {
+            if (inputStudentName == null)
+            {
+                this.studentName = "No Student Name";
+            }
+
+            this.studentName = inputStudentName;
+        }
+
+        public Student(string inputStudentName,
+            string inputSubjectName, int inputSubjectGrade)
+        {
+            this.studentName = inputStudentName;
+            AddGrade(inputSubjectName, inputSubjectGrade);
+        }
+
+        private bool HasSubject(string inputSubjectName)
+        {
+            foreach (var t in studentSubjects)
+            { if (t.IsEqual(inputSubjectName)) return true; }
+
+            return false;
+        }
+
+        public void AddGrade(string subjectName, int subjectGrade)
+        {
+            if (!HasSubject(subjectName))
+            {
+                Array.Resize(ref studentSubjects, studentSubjects.Length + 1);
+                studentSubjects[studentSubjects.Length - 1] = new Subject(subjectName, subjectGrade);
+            }
+            else
+            { UpdateExistingSubjectWithGrade(subjectName, subjectGrade); }
+        }
+
+        public bool IsEqual(string inputName)
+        { return studentName == inputName; }
+
+        public decimal GetAverage()
+        {
+            var totalAverageOutput = 0M;
+            foreach (var currentStudentSubject in studentSubjects)
+            { totalAverageOutput += currentStudentSubject.GetAverage(); }
+
+            if (totalAverageOutput == 0 || studentSubjects.Length == 0) return 0M; 
+
+            return decimal.Round(totalAverageOutput / studentSubjects.Length, 1);
+        }
+
+        private void UpdateExistingSubjectWithGrade(string subjectName, int subjectGrade)
+        {
+            foreach (var existingSubject in studentSubjects)
+            {
+                if (existingSubject.IsEqual(subjectName))
+                { existingSubject.AddGrade(subjectGrade); }
+            }
+        }
+    }
+}
---
+++ VirtualCatalogTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Xunit;
+
+namespace EncapsulationProj
+{
+    public class VirtualCatalogTest
+    {
+        [Fact]
+        public void ShouldReturnStudentAtTheGivenPosition()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("FirstTestStudent", "Matematica", 9);
+            var actual = catalog.StudentAtPosition(0);
+            Assert.True(actual.IsEqual("FirstTestStudent"));
+        }
+
+        [Fact]
+        public void AddGradeUpdateStudentIfAlreadyPresentInList()
+        {
+            var catalog = new VirtualCatalog();
+
+            catalog.AddGrade("FirstStudent", "Math", 5);
+            catalog.AddGrade("FirstStudent", "Biology", 4);
+
+            var expectedValue = 4.5M;
+            Assert.Equal(expectedValue, catalog.StudentAtPosition(0).GetAverage());
+        }
+
+        [Fact]
+        public void AddGradeInsertsTwoEntriesForTwoDifferentNames()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("FirstStudent", "Math", 4);
+            catalog.AddGrade("SecondStudent", "Biology", 5);
+
+            var expectedValue = 5;
+            Assert.Equal(expectedValue, catalog.StudentAtPosition(0).GetAverage());
+        }
+
+        [Fact]
+        public void AddGradesUpdatesEntriesForMultipleInsertedGradesForSameSubject()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("FirstStudent", "Math", 4);
+            catalog.AddGrade("SecondStudent", "Biology", 3);
+            catalog.AddGrade("FirstStudent", "Math", 5);
+            catalog.AddGrade("SecondStudent", "Biology", 6);
+
+            var firstExpectedValue = 4.5M;
+            var secondExpectedValue = 4.5M;
+
+            Assert.Equal(firstExpectedValue, catalog.StudentAtPosition(0).GetAverage());
+            Assert.Equal(secondExpectedValue, catalog.StudentAtPosition(1).GetAverage());
+        }
+
+        [Fact]
+        public void StudentAtPositionReturnsTopStudentFromMultipleStudentsAndSigleSubjects()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("FirstStudent", "Math", 9);
+            catalog.AddGrade("SecondStudent", "Biology", 2);
+            catalog.AddGrade("ThirdStudent", "Physics", 10);
+
+            Assert.True(catalog.StudentAtPosition(0).IsEqual("ThirdStudent"));
+        }
+
+        [Fact]
+        public void StudentAtPositionReturnsThirdValueFromMultipleStudents()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("Gicu", "Matematica", 8);
+            catalog.AddGrade("Dumitru", "Biologie", 4);
+            catalog.AddGrade("Mihai", "Romana", 8);
+            catalog.AddGrade("Ion", "Matematica", 3);
+            catalog.AddGrade("Zenovia", "Chimie", 9);
+            catalog.AddGrade("Gicu", "Biologie", 7);
+            catalog.AddGrade("Mihai", "Chimie", 10);
+            catalog.AddGrade("Gicu", "Matematica", 4);
+
+            Assert.True(catalog.StudentAtPosition(2).IsEqual("Gicu"));
+        }
+
+        [Fact]
+        public void IndexOfNameReturnsNullForNotExistentInput()
+        {
+            var catalog = new VirtualCatalog();
+            var expectedValue = -1;
+            Assert.Equal(expectedValue, catalog.IndexOfName(""));
+        }
+
+        [Fact]
+        public void IndexOfNameReturnsFirstIndexFromSortedValues()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("FirstStudent", "Math", 4);
+            catalog.AddGrade("SecondStudent", "Biologie", 10);
+            catalog.AddGrade("ThirdStudent", "Chimie", 3);
+            var expectedValue = 0;
+
+            Assert.Equal(expectedValue, catalog.IndexOfName("SecondStudent"));
+        }
+
+        [Fact]
+        public void IndexOfNameReturnsThirdIndexFromSortedValues()
+        {
+            var catalog = new VirtualCatalog();
+            catalog.AddGrade("Gicu", "Matematica", 8);
+            catalog.AddGrade("Dumitru", "Biologie", 4);
+            catalog.AddGrade("Mihai", "Romana", 8);
+            catalog.AddGrade("Ion", "Matematica", 3);
+            catalog.AddGrade("Zenovia", "Chimie", 9);
+            catalog.AddGrade("Gicu", "Biologie", 7);
+            catalog.AddGrade("Mihai", "Chimie", 10);
+            catalog.AddGrade("Gicu", "Matematica", 4);
+            var expectedValue = 2;
+
+            Assert.Equal(expectedValue, catalog.IndexOfName("Gicu"));
+        }
+    }
+}
